15342825af5891708d3b6149b087a5745bdef2d4
shindekalpesharun <shindekalpesharun@gmail.com>
2021-12-17 22:26:05 +0530

    data passing and for loop in vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello {{ name }}</h1>
+    <p>In this series, we will use the following framewrok</p>
+    <ul>
+        <li v-for="data of framework" v-text="data"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props:{
+        name:String,
+        framework:Array,
+    }
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia::render('Home', [
+        'name' => 'Shinde',
+        'framework' => ['laravel', "vue", "inertia"]
+    ]);
 });
