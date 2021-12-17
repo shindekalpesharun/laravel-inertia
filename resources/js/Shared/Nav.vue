e26b96238d91fb9c3c7bce0107cc0f28f4a45ce3
shindekalpesharun <shindekalpesharun@gmail.com>
2021-12-18 01:08:05 +0530

    ep 8 active link

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,17 +1,30 @@
 <template>
     <nav class="mt-6">
         <ul class="list-disc">
-            <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
-            <li><Link href="/users" class="text-blue-500 hover:underline">User</Link></li>
-            <li><Link href="/settings" class="text-blue-500 hover:underline">Settings</Link></li>
-            <li><Link href="/logout" :data="{foo:'bar'}" method="post" as="button" class="text-blue-500 hover:underline">log out</Link></li>
+            <li>
+                <NavLink href="/" :active="$page.component == 'Home'"
+                    >Home</NavLink
+                >
+            </li>
+            <li>
+                <NavLink href="/users" :active="$page.component == 'Users'"
+                    >Users</NavLink
+                >
+            </li>
+            <li>
+                <NavLink
+                    href="/settings"
+                    :active="$page.component == 'Settings'"
+                    >Settings</NavLink
+                >
+            </li>
         </ul>
     </nav>
 </template>
 
 <script>
-import{ Link }from '@inertiajs/inertia-vue3'
+import NavLink from "../Shared/NavLink";
 export default {
-    components:{Link},
-}
+    components: { NavLink },
+};
 </script>
